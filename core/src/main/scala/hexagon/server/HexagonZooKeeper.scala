c78627974599c3c4d175ff8ef67d524e71812899
zhyea <zhangrui@zhyea.com>
2017-12-25 22:01:01 +0800

    调整

--- core/src/main/scala/hexagon/server/HexagonZooKeeper.scala
+++ core/src/main/scala/hexagon/server/HexagonZooKeeper.scala
@@ -11,61 +11,67 @@ import org.apache.zookeeper.Watcher.Event
 
 class HexagonZooKeeper(val config: HexagonConfig) extends Logging {
 
-	val brokerIdPath = config.brokerIdsPath + "/" + config.brokerId
+  val brokerIdPath = config.brokerIdsPath + "/" + config.brokerId
 
-	var zkClient: ZkClient = null
+  var zkClient: ZkClient = null
 
-	val lock = new Object()
+  val lock = new Object()
 
-	def startup() {
-		info("Connecting to ZK: {}.", config.zkHost)
-		zkClient = new ZkClient(config.zkHost,
-			config.zkSessionTimeoutMs,
-			config.zkConnectionTimeoutMs,
-			ZkStringSerializer)
+  def startup() {
+    info("Connecting to ZK: {}.", config.zkHost)
+    zkClient = new ZkClient(config.zkHost,
+      config.zkSessionTimeoutMs,
+      config.zkConnectionTimeoutMs,
+      ZkStringSerializer)
 
-		zkClient.subscribeStateChanges(new SessionExpireListener)
-	}
-
-
-	def registerBrokerInZk(): Unit = {
-		info("Registering broker:{}. ", brokerIdPath)
-		val hostName = if (null == config.hostName) InetAddress.getLocalHost.getHostAddress else config.hostName
-		val creatorId: String = hostName + "-" + System.currentTimeMillis()
-		val broker = new Broker(config.brokerId, creatorId, hostName, config.port)
-		try {
-			ZKUtils.createEphemeralPathExpectConflict(zkClient, brokerIdPath, broker.getZkString())
-		} catch {
-			case e: ZkNodeExistsException => info("A broker is already registered on the path {}.", brokerIdPath)
-		}
-		info("Registering broker {} succeeded with {}", brokerIdPath, broker)
-	}
+    zkClient.subscribeStateChanges(new SessionExpireListener)
+  }
 
 
-	def registerTopicInZkInternal(topic: String): Unit = {
-
-	}
-
-
-	/**
-	  * When we get a SessionExpire event, we lost all ephemeral nodes and zkClient has reestablished a connect for us.
-	  * We need to re-register the broker in the broker registry.
-	  */
-	class SessionExpireListener extends IZkStateListener with Logging {
-
-		override def handleSessionEstablishmentError(error: Throwable): Unit = ???
-
-		override def handleStateChanged(state: Event.KeeperState): Unit = ???
+  def registerBrokerInZk(): Unit = {
+    info("Registering broker:{}. ", brokerIdPath)
+    val hostName = if (null == config.hostName) InetAddress.getLocalHost.getHostAddress else config.hostName
+    val creatorId: String = hostName + "-" + System.currentTimeMillis()
+    val broker = new Broker(config.brokerId, creatorId, hostName, config.port)
+    try {
+      ZKUtils.createEphemeralPathExpectConflict(zkClient, brokerIdPath, broker.getZkString())
+    } catch {
+      case e: ZkNodeExistsException => info("A broker is already registered on the path {}.", brokerIdPath)
+    }
+    info("Registering broker {} succeeded with {}", brokerIdPath, broker)
+  }
 
 
-		/**
-		  * Called after the ZooKeeper session has expired and a new session has been created.
-		  * You would have to re-create any ephemeral nodes here.
-		  */
-		override def handleNewSession(): Unit = {
-			info("Re-registering broker info in ZK for broker {}.", config.brokerId)
+  /**
+    * 注册topic
+    */
+  def registerTopicInZkInternal(topic: String): Unit = {
+    val brokerTopicPath = config.brokerTopicsPath + "/" + topic + "/" + config.brokerId
+    info("Begin registering topic {} with {} replicas.", brokerTopicPath, config.replicaNum)
+    ZKUtils.createEphemeralPathExpectConflict(zkClient, brokerTopicPath, config.replicaNum.toString)
+    info("End registering topic {}.", brokerTopicPath)
+  }
 
-		}
-	}
+
+  /**
+    * When we get a SessionExpire event, we lost all ephemeral nodes and zkClient has reestablished a connect for us.
+    * We need to re-register the broker in the broker registry.
+    */
+  class SessionExpireListener extends IZkStateListener with Logging {
+
+    override def handleSessionEstablishmentError(error: Throwable): Unit = ???
+
+    override def handleStateChanged(state: Event.KeeperState): Unit = ???
+
+
+    /**
+      * Called after the ZooKeeper session has expired and a new session has been created.
+      * You would have to re-create any ephemeral nodes here.
+      */
+    override def handleNewSession(): Unit = {
+      info("Re-registering broker info in ZK for broker {}.", config.brokerId)
+
+    }
+  }
 
 }
