86a2d70b445b208b6b28c11bd44a80ab1f8d8bbd
zhyea <zhangrui@zhyea.com>
2018-01-09 22:29:46 +0800

    尝试实现简单的NIOServer

--- core/src/main/scala/hexagon/network/SocketServer.scala
+++ core/src/main/scala/hexagon/network/SocketServer.scala
@@ -1,5 +1,25 @@
 package hexagon.network
 
-class SocketServer() {
+import java.net.InetSocketAddress
+import java.nio.channels.ServerSocketChannel
+
+class SocketServer(private val port: Int) {
+
+  private val serverSocketChannel: ServerSocketChannel = ServerSocketChannel.open()
+
+  def start(): Unit = {
+    serverSocketChannel.socket().bind(new InetSocketAddress(port))
+    while (true) {
+      val socketChannel = serverSocketChannel.accept()
+      if (null != socketChannel) {
+        // do something
+      }
+    }
+  }
+
+
+  def shutdown(): Unit = {
+    serverSocketChannel.close()
+  }
 
 }
--- core/src/main/scala/hexagon/tools/Logging.scala
+++ core/src/main/scala/hexagon/tools/Logging.scala
@@ -1,8 +1,25 @@
 package hexagon.tools
 
+import org.slf4j.{Logger, LoggerFactory}
+
 class Logging {
 
+  private val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
+  private[hexagon] def trace(format: String, args: Any*) = {
+    logger.trace(format, args)
+  }
 
+  private[hexagon] def info(format: String, args: Any*) = {
+    logger.trace(format, args)
+  }
+
+  private[hexagon] def warn(format: String, args: Any*) = {
+    logger.trace(format, args)
+  }
+
+  private[hexagon] def error(format: String, args: Any*) = {
+    logger.trace(format, args)
+  }
 
 }
