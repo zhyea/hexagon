334e55b2fb89cd3b334c69d3e382f95cfdc6b64f
zhyea <zhangrui@zhyea.com>
2018-01-10 22:48:31 +0800

    尝试实现简单的NIOServer

--- core/src/main/scala/hexagon/tools/Logging.scala
+++ core/src/main/scala/hexagon/tools/Logging.scala
@@ -6,19 +6,19 @@ class Logging {
 
   private val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
-  private[hexagon] def trace(format: String, args: Any*) = {
+  def trace(format: String, args: Any*) = {
     logger.trace(format, args)
   }
 
-  private[hexagon] def info(format: String, args: Any*) = {
+  def info(format: String, args: Any*) = {
     logger.trace(format, args)
   }
 
-  private[hexagon] def warn(format: String, args: Any*) = {
+  def warn(format: String, args: Any*) = {
     logger.trace(format, args)
   }
 
-  private[hexagon] def error(format: String, args: Any*) = {
+  def error(format: String, args: Any*) = {
     logger.trace(format, args)
   }
 
--- sample/src/main/scala/hexagon/server/nio/NioServer.scala
+++ sample/src/main/scala/hexagon/server/nio/NioServer.scala
@@ -1,15 +1,81 @@
 package hexagon.server.nio
 
+import java.net.InetSocketAddress
+import java.nio.channels.{SelectionKey, Selector, ServerSocketChannel, SocketChannel}
+import java.util.concurrent.atomic.AtomicBoolean
+
 class NioServer(private val port: Int) {
 
 
-	def start(): Unit = {
+  private val isRunning: AtomicBoolean = new AtomicBoolean(false)
 
-	}
+  private var selector: Selector = null
+  private var serverChannel: ServerSocketChannel = null
+
+  def init(): Unit = {
+    try {
+      selector = Selector.open
+      serverChannel = ServerSocketChannel.open
+
+      serverChannel.configureBlocking(true)
+      serverChannel.socket().bind(new InetSocketAddress(port), 1024)
+
+      serverChannel.register(selector, SelectionKey.OP_ACCEPT)
+
+      isRunning.set(true)
+
+      println("Server started. Port is : " + port)
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+  }
 
 
-	def handle(): Unit = {
+  def start(): Unit = {
+    init()
 
-	}
+    while (isRunning.get()) {
+      selector.select(1000)
+      val keys = selector.selectedKeys()
+      val iter = keys.iterator()
 
+      var key: SelectionKey = null
+      while (iter.hasNext) {
+        key = iter.next()
+        iter.remove()
+      }
+    }
+
+    if (null != selector)
+      selector.close()
+  }
+
+
+  def stop(): Unit = {
+    isRunning.set(false)
+  }
+
+  def handle(key: SelectionKey): Unit = {
+    if (key.isValid) {
+      if (key.isAcceptable) {
+        accept(key)
+      }
+      if (key.isReadable) {
+
+      }
+    }
+  }
+
+
+  def accept(key: SelectionKey): Unit = {
+    val ssc = key.channel().asInstanceOf[ServerSocketChannel]
+    val sc = ssc.accept()
+    sc.configureBlocking(true)
+    sc.register(selector, SelectionKey.OP_READ)
+  }
+
+
+  def read(key: SelectionKey): Unit = {
+    val sc = key.channel().asInstanceOf[SocketChannel]
+  }
 }
