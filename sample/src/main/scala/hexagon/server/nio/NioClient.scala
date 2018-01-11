e4db061c308e7cb1b626940c1653ae08e71bab18
zhyea <zhangrui@zhyea.com>
2018-01-12 07:19:06 +0800

    尝试实现简单的NIOServer

--- sample/src/main/scala/hexagon/server/nio/NioClient.scala
+++ sample/src/main/scala/hexagon/server/nio/NioClient.scala
@@ -1,9 +1,26 @@
 package hexagon.server.nio
 
+import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+import java.nio.channels.{SelectionKey, Selector, ServerSocketChannel, SocketChannel}
+
 class NioClient(private val host: String,
                 private val port: Int) {
 
+  private var socketChannel: SocketChannel = null
 
+  {
+    socketChannel = SocketChannel.open()
+    socketChannel.configureBlocking(false)
+    socketChannel.connect(new InetSocketAddress(host, port))
+  }
 
+  def start(): Unit = {
+    val bytes: Array[Byte] = "message from client.".getBytes()
+    val buffer = ByteBuffer.allocate(bytes.length)
+    buffer.put(bytes)
+    buffer.flip()
+    socketChannel.write(buffer)
+  }
 
 }
