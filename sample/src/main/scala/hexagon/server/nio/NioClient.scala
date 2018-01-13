9909fe3d53bcff5e2b0905853852987ec9f2966b
zhyea <zhangrui@zhyea.com>
2018-01-13 10:09:09 +0800

    尝试实现简单的NIOServer

--- sample/src/main/scala/hexagon/server/nio/NioClient.scala
+++ sample/src/main/scala/hexagon/server/nio/NioClient.scala
@@ -3,24 +3,86 @@ package hexagon.server.nio
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.channels.{SelectionKey, Selector, ServerSocketChannel, SocketChannel}
+import java.util.concurrent.atomic.AtomicBoolean
 
 class NioClient(private val host: String,
                 private val port: Int) {
 
+  private var selector: Selector = null
   private var socketChannel: SocketChannel = null
+  private val isRunning = new AtomicBoolean(false)
 
   {
+    selector = Selector.open()
     socketChannel = SocketChannel.open()
     socketChannel.configureBlocking(false)
-    socketChannel.connect(new InetSocketAddress(host, port))
+    isRunning.set(true)
+
+    println("Client started.")
   }
 
   def start(): Unit = {
-    val bytes: Array[Byte] = "message from client.".getBytes()
+    if (!socketChannel.connect(new InetSocketAddress(host, port))) {
+      socketChannel.register(selector, SelectionKey.OP_CONNECT)
+    }
+
+    while (isRunning.get()) {
+      selector.select(2000)
+      val keys = selector.selectedKeys()
+      val itr = keys.iterator()
+      while (itr.hasNext) {
+        val key = itr.next()
+        itr.remove()
+        handle(key)
+      }
+    }
+
+    if (null != selector) selector.close()
+  }
+
+
+  private def handle(key: SelectionKey): Unit = {
+    if (key.isValid) {
+      if (key.isConnectable) {
+        val sc = key.channel().asInstanceOf[SocketChannel]
+        if (!sc.finishConnect()) {
+          System.exit(1)
+        }
+      }
+      if (key.isReadable) {
+        read(key)
+      }
+    }
+  }
+
+
+  private def read(key: SelectionKey): Unit = {
+    val sc = key.channel().asInstanceOf[SocketChannel]
+    val buffer = ByteBuffer.allocate(1024)
+    val read = sc.read(buffer)
+    if (read > 0) {
+      buffer.flip()
+      val bytes = new Array[Byte](buffer.remaining())
+      buffer.get(bytes)
+      println(new String(bytes))
+    } else if (read < 0) {
+      key.cancel()
+      sc.close()
+    }
+  }
+
+
+  private def write(sc: SocketChannel, message: String): Unit = {
+    val bytes: Array[Byte] = message.getBytes()
     val buffer = ByteBuffer.allocate(bytes.length)
     buffer.put(bytes)
     buffer.flip()
-    socketChannel.write(buffer)
+    sc.write(buffer)
+  }
+
+  def send(message: String): Unit = {
+    socketChannel.register(selector, SelectionKey.OP_READ)
+    write(socketChannel, message)
   }
 
 }
--- sample/src/main/scala/hexagon/server/nio/NioTest.scala
+++ sample/src/main/scala/hexagon/server/nio/NioTest.scala
@@ -6,7 +6,15 @@ object NioTest extends App {
 
   val server = new NioServer(8089)
 
+  val client = new NioClient("127.0.0.1", 8089)
+
   new Thread(() => server.start()).start()
+  new Thread(() => client.start()).start()
+
+
+  TimeUnit.SECONDS.sleep(10)
+
+  client.send("A message from client.")
 
   TimeUnit.SECONDS.sleep(120)
 
