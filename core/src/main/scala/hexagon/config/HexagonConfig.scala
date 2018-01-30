e2063a2b6562a6fb36c8b386f1a3f7eb973523a4
zhyea <zhangrui@zhyea.com>
2018-01-30 22:31:49 +0800

    尝试实现简单的NIOServer

--- core/src/main/scala/hexagon/config/HexagonConfig.scala
+++ core/src/main/scala/hexagon/config/HexagonConfig.scala
@@ -9,5 +9,13 @@ private[hexagon] class HexagonConfig(props: Properties) extends ZooKeeperConfig(
 
   val port: Int = PropKit.getInt(props, "port", 9093)
 
+  val host: String = PropKit.getString(props, "host", "127.0.0.1")
+
+  val numNetworkThreads: Int = PropKit.getInt(props, "num.network.threads", Runtime.getRuntime.availableProcessors())
+
+  val socketSendBuffer: Int = PropKit.getInt(props, "socket.send.buffer", 1024)
+
+  val socketReceiveBuffer: Int = PropKit.getInt(props, "socket.receive.buffer", 1024)
+
 
 }
--- core/src/main/scala/hexagon/server/HexagonServer.scala
+++ core/src/main/scala/hexagon/server/HexagonServer.scala
@@ -5,11 +5,31 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 import hexagon.config.HexagonConfig
 import hexagon.network.SocketServer
+import hexagon.tools.Logging
 
-class HexagonServer(val config: HexagonConfig) {
+class HexagonServer(val config: HexagonConfig) extends Logging {
 
   private val isRunning: AtomicBoolean = new AtomicBoolean(false)
   private val shutdownLatch: CountDownLatch = new CountDownLatch(1)
-  private var socketServer: SocketServer = null
+
+  private var socketServer: SocketServer = _
+
+  def startup(): Unit = {
+    isRunning.set(true)
+  }
+
+
+  def awaitShutdown(): Unit = shutdownLatch.await()
+
+  def shutdown(): Unit = {
+    val canShutdown = isRunning.compareAndSet(true, false)
+    if (canShutdown) {
+      info("Shutting down hexagon server.")
+      if (null != socketServer) socketServer.shutdown()
+      shutdownLatch.countDown()
+      info("Shutdown hexagon server completely.")
+    }
+
+  }
 
 }
