fa1f5b2c7819f9f951f6c883330b82cbf11114df
zhyea <zhangrui@zhyea.com>
2018-02-01 07:10:06 +0800

    尝试实现简单的NIOServer

--- core/src/main/scala/hexagon/Hexagon.scala
+++ core/src/main/scala/hexagon/Hexagon.scala
@@ -1,16 +1,35 @@
-import hexagon.tools.Logging
+package hexagon
+
+import hexagon.server.{HexagonServer, HexagonServerStartable}
+import hexagon.tools.{Logging, PropKit}
+import hexagon.config.HexagonConfig
 
 object Hexagon extends Logging {
 
 
   def main(args: Array[String]): Unit = {
+
+    if (args.length != 1) {
+      println(s"Usage: java [options] ${classOf[HexagonServer].getSimpleName} server.properties")
+      System.exit(1)
+    }
+
     try {
+      val props = PropKit.load(args(0))
+      val config = new HexagonConfig(props)
+
+      val serverStartable = new HexagonServerStartable(config)
+      Runtime.getRuntime
+        .addShutdownHook(new Thread(() => serverStartable.shutdown()))
+
+      serverStartable.startup()
+      serverStartable.awaitShutdown()
 
     } catch {
       case e: Exception => error("Error during hexagon startup ", e)
     }
 
-
+    System.exit(1)
   }
 
 
