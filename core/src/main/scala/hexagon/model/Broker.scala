9cbb60689486115b1d28b1df5b112e480e9695de
zhyea <zhangrui@zhyea.com>
2019-01-24 19:48:30 +0800

    alter

---
+++ core/src/main/scala/hexagon/model/Broker.scala
@@ -0,0 +1,19 @@
+package hexagon.model
+
+import hexagon.utils.Maths._
+
+case class Broker(id: Int, host: String, port: Int) {
+
+
+  override def toString: String = s"id:$id,host:$host,post:$port"
+
+  override def equals(obj: Any): Boolean = {
+    obj match {
+      case null => false
+      case b: Broker => id == b.id && host == b.host && port == b.port
+      case _ => false
+    }
+  }
+
+  override def hashCode(): Int = hashcode(id, host, port)
+}
---
+++ core/src/main/scala/hexagon/model/TopicAndPartition.scala
@@ -0,0 +1,7 @@
+package hexagon.model
+
+case class TopicAndPartition(topic: String, partition: Int) {
+
+
+  override def toString: String = s"$topic,$partition"
+}
