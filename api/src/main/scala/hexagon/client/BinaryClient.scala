24faee8e6cacbe2b8e5813a9494ca3a70c959f93
zhyea <zhangrui@zhyea.com>
2019-04-09 11:14:53 +0800

    alter

--- api/src/main/scala/hexagon/client/BinaryClient.scala
+++ api/src/main/scala/hexagon/client/BinaryClient.scala
@@ -1,36 +1,97 @@
 package hexagon.client
 
+import java.util.concurrent.atomic.AtomicBoolean
+
 import hexagon.api.PutRequest
-import hexagon.network.BlockingChannel
+import hexagon.network.{BlockingChannel, Receive, RequestOrResponse}
 import hexagon.protocol.Entity
 import hexagon.serializer.Encoder
+import hexagon.tools.Logging
+import hexagon.utils.NetUtils._
 
 class BinaryClient[T](val host: String,
                       val port: Int,
-                      val encoder: Encoder[T]) {
+                      val readBufferSize: Int,
+                      val writeBufferSize: Int,
+                      val socketTimeout: Int,
+                      val encoder: Encoder[T]) extends Logging {
 
-  private val blockingChannel: BlockingChannel = new BlockingChannel(host, port)
+  private val blockingChannel: BlockingChannel =
+    new BlockingChannel(host, port, readBufferSize, writeBufferSize, socketTimeout)
 
+  private val closed: AtomicBoolean = new AtomicBoolean(true)
 
   private val lock: Object = new Object()
 
 
-  def connect(): Unit = {
+  private def connect(): BlockingChannel = {
     lock synchronized {
+      close()
       blockingChannel.connect()
+      blockingChannel
     }
   }
 
 
-  def disconnect(): Unit = {
+  private def disconnect(): Unit = {
+    debug(s"Disconnecting from ${formatAddress(host, port)}")
     lock synchronized {
       blockingChannel.disconnect()
     }
   }
 
 
+  private def reconnect(): Unit = {
+    disconnect()
+    connect()
+  }
+
+
+  private def getOrMakeConnection(): Unit = {
+    if (!isClosed && !blockingChannel.isConnected) {
+      connect()
+    }
+  }
+
+
+  def close(): Unit = {
+    lock synchronized {
+      disconnect()
+      closed.set(true)
+    }
+  }
+
+  def isClosed: Boolean = closed.get()
+
+
+  private def sendRequest(request: RequestOrResponse): Receive = {
+    lock synchronized {
+      var response: Receive = null
+      try {
+        getOrMakeConnection()
+        blockingChannel.send(request)
+        response = blockingChannel.receive()
+      } catch {
+        case e: Throwable =>
+          info(s"Reconnect due to socket error: ${e.toString}")
+          try {
+            reconnect()
+            blockingChannel.send(request)
+            response = blockingChannel.receive()
+          } catch {
+            case e: Throwable =>
+              disconnect()
+              throw e
+          }
+      }
+      response
+    }
+  }
+
+
   def put(record: Record[Entity]): Boolean = {
     val req = PutRequest(record.topic, record.value)
+    val
     blockingChannel.send(req)
     ???
   }
