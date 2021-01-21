75a7013b641a0f41f93d5c16d10df694ab65560b
zhyea <zhangrui@zhyea.com>
2021-01-21 08:26:56 +0800

    alter
    Change-Id: I00194e15de9b5c72f802094f4120c94a1f604635

--- server/src/main/scala/hexagon/server/HexagonServer.scala
+++ server/src/main/scala/hexagon/server/HexagonServer.scala
@@ -2,17 +2,17 @@ package hexagon.server
 
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicBoolean
-
 import hexagon.config.HexagonConfig
 import hexagon.network
 import hexagon.tools.Logging
+import io.vertx.core.net.NetServer
 
 class HexagonServer(val config: HexagonConfig) extends Logging {
 
 	private val isRunning: AtomicBoolean = new AtomicBoolean(false)
 	private val shutdownLatch: CountDownLatch = new CountDownLatch(1)
 
-	private var socketServer: network.SocketServer = _
+	private var socketServer: NetServer = _
 
 	def startup(): Unit = {
 		info("Hexagon server is starting.")
